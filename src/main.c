45f50b6190d965ea7c7067383dce23f71cb12a60
Angel Martin <AMartin12122019@Outlook.com>
2025-09-28 00:32:33 -0700

    have my project not deadname me

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*
         Vita Kernel Plugin Installer
-    Copyright (C) 2025  Angel Martin
+    Copyright (C) 2025  Xenia Nash
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
